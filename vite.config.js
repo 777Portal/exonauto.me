c18039763154adbe4a09179b920a97906f789ffc
exon <throwawayact69xd@gmail.com>
2025-04-25 08:05:38 -0400

    Vite just cuz

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+
+export default defineConfig({
+  root: "./app/",
+  plugins: [
+      ViteMinifyPlugin(),
+  ],
+  
+  build: {
+    rollupOptions: {
+      manifest: true,
+      input: {
+        'main': 'index.html',
+        'blog': '/pages/blog/index.html'
+      },
+      outDir: '/dist',
+    },
+  },
+});
